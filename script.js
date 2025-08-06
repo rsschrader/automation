7e39cfa191d8fe83ba1d004142f77ec923d7503b
Sebastian Schrader <sebastian.schrader@mvwc.com>
2025-08-06 13:43:48 -0400

    qa

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function attachScriptRunnerButtonListener() {
-  const button = document.getElementById("run-custom-script");
-  const messageBox = document.getElementById("script-response-message");
+	const button = document.getElementById("run-custom-script");
+	const messageBox = document.getElementById("script-response-message");
 
-  if (button) {
-    button.addEventListener("click", function () {
-      fetch('https://dcmcobwasqld01.ad.mvwcorp.com:8445/api/v1/ping', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          messageBox.innerText = data.message || "Action completed!";
-          messageBox.style.color = "green";
-        })
-        .catch(error => {
-          messageBox.innerText = "Error: " + error;
-          messageBox.style.color = "red";
-        });
-    });
-  } else {
-    // Retry after a short delay
-    setTimeout(attachScriptRunnerButtonListener, 200);
-  }
+	if (button) {
+		button.addEventListener("click", function() {
+			fetch('https://jsonplaceholder.typicode.com/posts/1', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+				.then(response => response.json())
+				.then(data => {
+					messageBox.innerText = data.message || "Action completed!";
+					messageBox.style.color = "green";
+				})
+				.catch(error => {
+					messageBox.innerText = "Error: " + error;
+					messageBox.style.color = "red";
+				});
+		});
+	} else {
+		// Retry after a short delay
+		setTimeout(attachScriptRunnerButtonListener, 200);
+	}
 }
 
 document.addEventListener("DOMContentLoaded", attachScriptRunnerButtonListener);
