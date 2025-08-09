7b76cc7daca99a10af5325289e9af0baa51b0408
Sebastian Schrader <sebastian.schrader@mvwc.com>
2025-08-08 21:54:45 -0400

    qa

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
 function attachScriptRunnerButtonListener() {
 	const button = document.getElementById("run-custom-script");
 	const messageBox = document.getElementById("script-response-message");
-	
+
 	if (button) {
 		button.addEventListener("click", function() {
 			const issueKey = window.AdaptavistBridgeContext?.context?.issueKey;
-			if (issueKey) {
-			  console.log("Issue key is:", issueKey);
-			  messageBox.innerText = `Issue key is: ${issueKey}`;
-				fetch(`https://mvwc-jira.atlassian.net/rest/api/3/issue/${issueKey}?fields=issuetype`)
-				  .then(response => {
-				    if (!response.ok) throw new Error(`HTTP ${response.status}`);
-				    return response.json();
-				  })
-				  .then(({ issueType }) => {
-				    console.log('Issue type:', issueType);
-				  })
-				  .catch(err => console.error('Failed to get issue type:', err));
-			} else {
-			  console.error("Issue key not found. Are you inside a ScriptRunner Cloud webPanel?");
-			}
-	
-			messageBox.innerText = "Calling API...";
-			messageBox.style.color = "gray";
-
-			fetch('https://dcmcobwasqld01.ad.mvwcorp.com:8445/api/v1/ping', {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			})
-			.then(response => {
-				if (!response.ok) {
-					throw new Error(`HTTP error ${response.status}`);
-				}
-				return response.json();
-			})
-			.then(data => {
-				messageBox.innerText = issueKey + " " + JSON.stringify(data, null, 2);
-				messageBox.style.color = "black";
-			})
-			.catch(error => {
-				console.error("Caught error in fetch:", error);
-				messageBox.innerText = "Fetch error: " + error.message;
-				messageBox.style.color = "red";
-			});
+			button.style.display = "none";
 		});
 	} else {
+		// Retry after a short delay
 		setTimeout(attachScriptRunnerButtonListener, 200);
 	}
 }
