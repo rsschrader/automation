c9c29e219bcb431a72a3dcde2cf6a4b4ee473f31
Sebastian Schrader <sebastian.schrader@mvwc.com>
2025-08-08 22:12:59 -0400

    qa

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function attachScriptRunnerButtonListener() {
 	const button = document.getElementById("run-custom-script");
+	const button2 = document.getElementById("run-custom-script2");
 	const messageBox = document.getElementById("script-response-message");
 
 	if (button) {
+		button.style.display = "none";
 		button.addEventListener("click", function() {
 			const issueKey = window.AdaptavistBridgeContext?.context?.issueKey;
-			button.style.display = "none";
+			button2.style.display = "block";
 		});
 	} else {
 		// Retry after a short delay
