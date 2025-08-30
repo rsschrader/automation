ac5da7d33873f3c0c73c36f39c90004948842991
rsschrader <code.schrader@gmail.com>
2025-08-30 14:10:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 
 function attachScriptRunnerButtonListener() {
-	const messageBox = document.getElementById("script-response-message");
-	const issueKey = window.AdaptavistBridgeContext?.context?.issueKey;
+    const messageBox = document.getElementById("script-response-message");
+    const issueKey = window.AdaptavistBridgeContext?.context?.issueKey;
 
-	const fetchWithTimeout = (url, options = {}, timeout = 300000) => {
-		return Promise.race([
-			fetch(url, options),
-			new Promise((_, reject) =>
-			setTimeout(() => reject(new Error("Request timed out")), timeout)
-			)
-		]);
-	};
+    const fetchWithTimeout = (url, options = {}, timeout = 300000) => {
+        return Promise.race([
+            fetch(url, options),
+            new Promise((_, reject) =>
+                setTimeout(() => reject(new Error("Request timed out")), timeout)
+            )
+        ]);
+    };
 
-	try {
-		const response = await fetchWithTimeout(
-			`https://dcmcobwasqld01.ad.mvwcorp.com:8445/api/v1/jira/type?JiraIssueKey=${issueKey}`,
-			{
-				method: 'GET',
-				headers: {
-				'Content-Type': 'application/json'
-				}
-			}
-		);
-		if (!response.ok) {
-	        throw new Error(`HTTP error! Status: ${response.status}`);
-	    }
+    const buttonMain = document.getElementById("run-test-main-script");
+    if (buttonMain) {
+        buttonMain.addEventListener("click", async function () {
+            try {
+            const response = await fetchWithTimeout(
+                `https://dcmcobwasqld01.ad.mvwcorp.com:8445/api/v1/jira/type?JiraIssueKey=${issueKey}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-	} catch (error) {
-	    console.error("Caught error in fetch:", error);
-	    messageBox.innerText = "Fetch error: " + error.message;
-	    messageBox.style.color = "red";
-	}
+            } catch (error) {
+                console.error("Caught error in fetch:", error);
+                messageBox.innerText = "Fetch error: " + error.message;
+                messageBox.style.color = "red";
+            }
+        });
+    }
 }
 document.addEventListener("DOMContentLoaded", attachScriptRunnerButtonListener);
